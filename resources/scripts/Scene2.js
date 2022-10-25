bd74b2e60bd2a18e07c6e53ce873b5f1a42a4c34
VishvakCh <95390151+chintavs@users.noreply.github.com>
2022-10-25 00:23:33 -0400

    completed part 4

--- resources/scripts/Scene2.js
+++ resources/scripts/Scene2.js
@@ -4,7 +4,7 @@ class Scene2 extends Phaser.Scene {
     }
 
     create() {
-        this.background = this.add.image(0,0,"background")
+        this.background = this.add.tileSprite(0,0,config.width,config.height,"background")
         this.background.setOrigin(0,0)
 
         this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship")
@@ -13,5 +13,25 @@ class Scene2 extends Phaser.Scene {
 
 
         this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
+    }
+
+    moveShip(ship, speed) {
+        ship.y += speed;
+        if (ship.y > config.height) {
+            this.resetShipPos(ship);
+        }
+    }
+
+    update () {
+        this.moveShip(this.ship1, 1);
+        this.moveShip(this.ship2, 2);
+        this.moveShip(this.ship3, 3);
+
+        this.background.tilePositionY -= 0.5
+    }
+
+    resetShipPos(ship) {
+        ship.y = 0;
+        ship.x = Phaser.Math.Between(0, config.width);
     }
 }
