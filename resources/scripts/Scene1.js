51de45fa8b97a3fbb623f13b6ec4b32456e2f34f
VishvakCh <95390151+chintavs@users.noreply.github.com>
2022-10-26 20:04:41 -0400

    completed part 5

--- resources/scripts/Scene1.js
+++ resources/scripts/Scene1.js
@@ -5,10 +5,31 @@ class Scene1 extends Phaser.Scene {
 
     preload() {
         this.load.image("background", "resources/images/background.png");
-        this.load.image("ship", "resources/images/ship.png");
-        this.load.image("ship2", "resources/images/ship2.png");
-        this.load.image("ship3", "resources/images/ship3.png");
-    }
+
+        // this.load.image("ship", "resources/image/ship.png");
+        // this.load.image("ship2", "resources/image/ship2.png");
+        // this.load.image("ship3", "resources/image/ship3.png");
+
+        this.load.spritesheet("ship", "resources/spritesheets/ship.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+
+        this.load.spritesheet("ship2", "resources/spritesheets/ship2.png", {
+            frameWidth: 32,
+            frameHeight: 16
+        });
+
+        this.load.spritesheet("ship3", "resources/spritesheets/ship3.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        });
+
+        this.load.spritesheet("explosion", "resources/spritesheets/explosion.png", {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+    } 
 
     create() {
         this.add.text(20, 20, "Loading game...");
