9b61e358f5031b3e1f7854524013b58f190c62ad
VishvakCh <95390151+chintavs@users.noreply.github.com>
2022-10-29 02:07:25 -0400

    completed part 9

--- resources/scripts/Scene2.js
+++ resources/scripts/Scene2.js
@@ -7,21 +7,22 @@ class Scene2 extends Phaser.Scene {
         this.background = this.add.tileSprite(0,0,config.width,config.height,"background")
         this.background.setOrigin(0,0)
 
-        // this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship")
-        // this.ship2 = this.add.image(config.width/2, config.height/2, "ship2")
-        // this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3")
+        this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship")
+        this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2")
+        this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3")
 
-        // this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship")
-        // this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2")
-        // this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3")
+        this.enemies = this.physics.add.group();
+        this.enemies.add(this.ship1);
+        this.enemies.add(this.ship2);
+        this.enemies.add(this.ship3);
 
-        // this.ship1.play("ship1_anim");
-        // this.ship2.play("ship2_anim");
-        // this.ship3.play("ship3_anim");
+        this.ship1.play("ship1_anim");
+        this.ship2.play("ship2_anim");
+        this.ship3.play("ship3_anim");
         
-        // this.ship1.setInteractive();
-        // this.ship2.setInteractive();
-        // this.ship3.setInteractive();
+        this.ship1.setInteractive();
+        this.ship2.setInteractive();
+        this.ship3.setInteractive();
 
         this.player = this.physics.add.sprite(config.width / 2 - 8, config.height - 64, "player");
         this.player.play("thrust");
@@ -30,70 +31,79 @@ class Scene2 extends Phaser.Scene {
 
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.projectiles = this.add.group();
-
-        // this.powerUps = this.physics.add.group();
+        this.powerUps = this.physics.add.group();
 
         
-        // var maxObjects = 4;
-        // for (let i = 0; i <= maxObjects; i++) {
-        //     var powerUp = this.physics.add.sprite(16, 16, "power-up")
-        //     this.powerUps.add(powerUp);
-        //     powerUp.setRandomPosition(0, 0, config.width, config.height)
+        var maxObjects = 4;
+        for (let i = 0; i <= maxObjects; i++) {
+            var powerUp = this.physics.add.sprite(16, 16, "power-up")
+            this.powerUps.add(powerUp);
+            powerUp.setRandomPosition(0, 0, config.width, config.height)
 
-        //     if (Math.random() > 0.5) {
-        //         powerUp.play("red");
-        //     } else {
-        //         powerUp.play("gray");
-        //     }
+            if (Math.random() > 0.5) {
+                powerUp.play("red");
+            } else {
+                powerUp.play("gray");
+            }
 
-        //     powerUp.setVelocity(100, 100);
-        //     powerUp.setCollideWorldBounds(true);
-        //     powerUp.setBounce(1);
-        // }
+            powerUp.setVelocity(100, 100);
+            powerUp.setCollideWorldBounds(true);
+            powerUp.setBounce(1);
+        }
 
         this.add.text(20, 20, "Playing game", {
             font: "25px Arial", 
             fill: "yellow"
         });
 
-        // this.input.on('gameobjectdown', this.destroyShip, this);
+        this.physics.add.collider(this.projectiles, this.powerUps, function(projectile, powerUp) {
+          projectile.destroy();
+        });
+
+        this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
+
+        this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
+
+        this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
+
+        this.input.on('gameobjectdown', this.destroyShip, this);
     }
 
-    // moveShip(ship, speed) {
-    //     ship.y += speed;
-    //     if (ship.y > config.height) {
-    //         this.resetShipPos(ship);
-    //     }
-    // }
-
+    
     update () {
-        // this.moveShip(this.ship1, 1);
-        // this.moveShip(this.ship2, 2);
-        // this.moveShip(this.ship3, 3);
-        var beams = [];
+        this.moveShip(this.ship1, 1);
+        this.moveShip(this.ship2, 2);
+        this.moveShip(this.ship3, 3);
 
         this.background.tilePositionY -= 0.5;
 
         this.movePlayerManager();
 
         if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
-            beams.push(this.shootBeam())
+            this.shootBeam();
         }
-
-        for (var i = 0; i < beams.length; i++) {
-            beams[i].update();
+        
+        for (var i = 0; i < this.projectiles.getChildren().length; i++) {
+          var beam = this.projectiles.getChildren()[i];
+          beam.update();
+        }
+    }
+    moveShip(ship, speed) {
+        ship.y += speed;
+        if (ship.y > config.height) {
+            this.resetShipPos(ship);
         }
     }
 
-    // resetShipPos(ship) {
-    //     ship.y = 0;
-    //     ship.x = Phaser.Math.Between(0, config.width);
-    // }
+    resetShipPos(ship) {
+          ship.y = 0;
+        ship.x = Phaser.Math.Between(0, config.width);
+    }
 
-    // destroyShip(pointer, gameObject) {
-    //     gameObject.setTexture("explode");
-    //     gameObject.play("explode");
-    // }
+    destroyShip(pointer, gameObject) {
+        gameObject.setTexture("explode");
+        gameObject.play("explode");
+    }
 
     movePlayerManager() {
         this.player.setVelocity(0);
@@ -113,6 +123,21 @@ class Scene2 extends Phaser.Scene {
 
     shootBeam() {
         var beam = new Beam(this)
-        return beam;
+        this.projectiles.add(beam);
+    }
+
+    pickPowerUp(player, powerUp) {
+      powerUp.disableBody(true, true)
+    }
+
+    hurtPlayer(player, enemy) {
+      this.resetShipPos(enemy);
+      player.x = config.width / 2 - 8;
+      player.y = config.height - 64;
+    }
+  
+    hitEnemy(projectile, enemy) {
+      projectile.destroy();
+      this.resetShipPos(enemy);
     }
 }
